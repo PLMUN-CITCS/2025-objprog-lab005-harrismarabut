3af4aa9e7219807c2b9c6d988c1879e0fe2204ba
harris <marabutharris_bsit@plmun.edu.ph>
2025-02-15 23:28:04 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 04

---
+++ MathLibraryDemo.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        double base = 3, exponent = 5;
+        double number = 16;
+        double valueToRound = 8.7;
+
+        System.out.println("Power: " + Math.pow(base, exponent));      
+        System.out.println("Square Root: " + Math.sqrt(number));       
+        System.out.println("Rounded Number: " + Math.round(valueToRound));  
+        System.out.println("Random Number: " + Math.random());         
+    }
+}
