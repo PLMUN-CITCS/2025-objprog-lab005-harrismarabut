b5490a8a18086045b694dbd286aa976d6101e5ec
Harrismarabut1 <marabutharris_bsit@plmun.edu.ph>
2025-02-16 23:50:02 +0800

    Update MathLibraryDemo.java

--- MathLibraryDemo.java
+++ MathLibraryDemo.java
@@ -1,12 +1,22 @@
-public class Main {
+public class MathFunctionsDemo {
     public static void main(String[] args) {
-        double base = 3, exponent = 5;
-        double number = 16;
-        double valueToRound = 8.7;
+      
+        double powerResult = Math.pow(2, 3); 
+        
+        
+        double squareRootResult = Math.sqrt(16); 
+        
+        
+        long roundedNumber = Math.round(5.7); 
+        
+        
+        double randomNumber = Math.random(); 
 
-        System.out.println("Power: " + Math.pow(base, exponent));      
-        System.out.println("Square Root: " + Math.sqrt(number));       
-        System.out.println("Rounded Number: " + Math.round(valueToRound));  
-        System.out.println("Random Number: " + Math.random());         
+        
+        System.out.println("Power: " + powerResult);
+        System.out.println("Square Root: " + squareRootResult);
+        System.out.println("Rounded Number: " + roundedNumber);
+        System.out.println("Random Number: " + randomNumber);
     }
 }
+
